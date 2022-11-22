b03f83ab6261dd4e2f65fa4abf01e14109a9fdcc
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-11-22 02:39:14 +0000

    updated function

--- src/welcome-app/main.go
+++ src/welcome-app/main.go
@@ -1,15 +1,27 @@
 package main
 
-import(
-	"fetch"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/users")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	log.Printf(sb)
+}
 
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then(response => response.json())
-  .then(json => console.log(json))
 
-/*import (
+/*
+import (
 	"fmt"
 	"html/template"
 	"net/http"
@@ -18,7 +30,7 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
 	
 )
 
-// Create a struct that holds information to be displayed in our HTML file
+ Create a struct that holds information to be displayed in our HTML file
 type Welcome struct {
 	Name string
 	Time string
@@ -35,19 +47,19 @@ type JsonNested struct {
 	NestedValue2 string `json:"nestedkey2"`
 }
 
-// Go application entrypoint
+ Go application entrypoint
  func main() {
 	fetch("https://jsonplaceholder.ir/users");{
 	
 		,then(response => response.json())
 		.then(json => console.log(json)),
 	}
-	//Instantiate a Welcome struct object and pass in some random information.
-	//We shall get the name of the user as a query parameter from the URL
+	Instantiate a Welcome struct object and pass in some random information.
+	We shall get the name of the user as a query parameter from the URL
 	 welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
 
-	//We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
-	// the relative path). We wrap it in a call to template.Must() which handles any errors and halts if there are fatal errors
+	We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
+	 the relative path). We wrap it in a call to template.Must() which handles any errors and halts if there are fatal errors
 
 	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 
@@ -64,27 +76,27 @@ type JsonNested struct {
 
 	
 
-	//Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
-	// a handle that looks in the static directory, go then uses the "/static/" as a url that our
-	//html can refer to when looking for our css and other files.
+	Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
+	 a handle that looks in the static directory, go then uses the "/static/" as a url that our
+	html can refer to when looking for our css and other files.
 
 	http.Handle("/static/", //final url can be anything
 		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("static")))) //Go looks in the relative "static" directory first using http.FileServer(), then matches it to a
-	//url of our choice as shown in http.Handle("/static/"). This url is what we need when referencing our css files
-	//once the server begins. Our html code would therefore be <link rel="stylesheet"  href="/static/stylesheet/...">
-	//It is important to note the url in http.Handle can be whatever we like, so long as we are consistent.
+			http.FileServer(http.Dir("static")))) Go looks in the relative "static" directory first using http.FileServer(), then matches it to a
+	url of our choice as shown in http.Handle("/static/"). This url is what we need when referencing our css files
+	once the server begins. Our html code would therefore be <link rel="stylesheet"  href="/static/stylesheet/...">
+	It is important to note the url in http.Handle can be whatever we like, so long as we are consistent.
 
-	//This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
-	// **** THIS IS THE MAIN PATH / 
+	This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
+	 **** THIS IS THE MAIN PATH / 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		//Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
+		Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
 		if name := r.FormValue("name"); name != "" {
 			welcome.Name = name
 		}
-		//If errors show an internal server error message
-		//I also pass the welcome struct to the welcome-template.html file.
+		If errors show an internal server error message
+		I also pass the welcome struct to the welcome-template.html file.
 		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -93,10 +105,9 @@ type JsonNested struct {
 		json.NewEncoder(w).Encode(JsonResp)
 	})
 
-	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
-	//Print any errors from starting the webserver using fmt
+	Start the web server, set the port to listen to 8080. Without a path it assumes localhost
+	Print any errors from starting the webserver using fmt
 	fmt.Println("Listening")
 	fmt.Println(http.ListenAndServe(":8080", nil))
-} */
-
-
+} 
+*/
